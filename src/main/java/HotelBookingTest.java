597573cb9015910d7f226afb338524ee79403cde
IndhumathiMohanraj <mathiiraj07@gmail.com>
2018-09-07 12:40:21 +0530

    Initialized the page factory elements

--- src/main/java/HotelBookingTest.java
+++ src/main/java/HotelBookingTest.java
@@ -3,13 +3,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class HotelBookingTest {
 
-    WebDriver driver = new ChromeDriver();
-
+    public WebDriver driver;
     @FindBy(linkText = "Hotels")
     private WebElement hotelLink;
 
@@ -25,29 +26,38 @@ public class HotelBookingTest {
     @Test
     public void shouldBeAbleToSearchForHotels() {
     	
-        setDriverPath();
+        try {
+			setDriverPath();
+			HotelBookingTest HBT = PageFactory.initElements(driver, HotelBookingTest.class);
 
-        driver.get("https://www.cleartrip.com/");
-        hotelLink.click();
+			driver.get("https://www.cleartrip.com/");
+			HBT.hotelLink.click();
 
-        localityTextBox.sendKeys("Indiranagar, Bangalore");
+			HBT.localityTextBox.sendKeys("Indiranagar, Bangalore");
 
-        new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
-        searchButton.click();
+			new Select(HBT.travellerSelection).selectByVisibleText("1 room, 2 adults");
+			HBT.searchButton.click();
 
-        driver.quit();
+			driver.quit();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
     }
 
     private void setDriverPath() {
         if (PlatformUtil.isMac()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver");
+            driver = new ChromeDriver();
         }
         if (PlatformUtil.isWindows()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+            driver = new ChromeDriver();
         }
         if (PlatformUtil.isLinux()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
+            driver = new ChromeDriver();
         }
     }
 
