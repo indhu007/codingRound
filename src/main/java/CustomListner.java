cb541440e7b29c950f430bb3cd4a1f404a4c61db
IndhumathiMohanraj <mathiiraj07@gmail.com>
2018-09-07 17:47:00 +0530

    logging

--- src/main/java/CustomListner.java
+++ src/main/java/CustomListner.java
@@ -1,29 +1,29 @@
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
 
-public class CustomListner extends TestListenerAdapter{
-   private int m_count = 0;
-	 
-   @Override
-   public void onTestFailure(ITestResult tr) {
-      log(tr.getName()+ "--Test method failed\n");
-   }
-	 
-   @Override
-   public void onTestSkipped(ITestResult tr) {
-      log(tr.getName()+ "--Test method skipped\n");
-   }
-	 
-   @Override
-   public void onTestSuccess(ITestResult tr) {
-      log(tr.getName()+ "--Test method success\n");
-   }
-	 
-   private void log(String string) {
-      System.out.print(string);
-      if (++m_count % 100 == 0) {
-         System.out.println("");
-      }
-   }
+public class CustomListner extends TestListenerAdapter {
+	private int m_count = 0;
+
+	@Override
+	public void onTestFailure(ITestResult tr) {
+		log(tr.getName() + "--Test method failed\n");
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult tr) {
+		log(tr.getName() + "--Test method skipped\n");
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult tr) {
+		log(tr.getName() + "--Test method success\n");
+	}
+
+	private void log(String string) {
+		System.out.print(string);
+		if (++m_count % 100 == 0) {
+			System.out.println("");
+		}
+	}
 
 }
